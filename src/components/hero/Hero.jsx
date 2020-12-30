32562e8a5fa2e91c480680760a118a0613efb397
falsepopsky <javicream@gmail.com>
2020-12-29 22:07:23 -0300

    fixed css, keyframes on hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { HeroStyles } from './hero-styled';
-import vagabondLogo from '../../assets/vagabond-logo.svg';
+import VagabondLogo from '../../assets/vagabond-logo.svg';
+import ArrowDown from '../../assets/arrow-down.svg';
 
 const HeroSection = () => {
   return (
     <HeroStyles>
       <Row as="section" className="vagabond-background">
-        <Col className="full-heigth"></Col>
+        <Col className="full-heigth">
+          <h1 className="hero-text-main">Vagabond Hiatus Chart</h1>
+          <figure>
+            <img
+              className="arrow-down-freepik"
+              src={ArrowDown}
+              alt="arrow down by Freepik"
+            />
+          </figure>
+        </Col>
       </Row>
       <Row as="section">
         <Col>
-          <img className="vagabond-logo" src={vagabondLogo} alt="logo" />
-          <h1 className="hero-text-main">by Takehiko Inoue</h1>
+          <img className="vagabond-logo" src={VagabondLogo} alt="logo" />
+          <h1 className="hero-text-sub">
+            Takehiko Inoue
+            <span>(井上 雄彦)</span>
+          </h1>
         </Col>
       </Row>
     </HeroStyles>
--- src/components/hero/hero-styled.js
+++ src/components/hero/hero-styled.js
@@ -1,5 +1,20 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import vagabond from '../../assets/vagabond-min.png';
+
+const bounce = keyframes`
+0% {
+  bottom: 40px;
+}
+50% {
+  bottom: 50px;
+}
+100% {
+  bottom: 40px;
+}`;
+
+const animationRule = css`
+  ${bounce} 1s infinite;
+`;
 
 export const HeroStyles = styled.div`
   .row {
@@ -9,14 +24,29 @@ export const HeroStyles = styled.div`
 
   .vagabond-background {
     width: 100%;
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)),
       url(${vagabond}) no-repeat center center fixed;
     background-size: cover;
     height: 100%;
   }
 
   .full-heigth {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 100vh;
+  }
+
+  .arrow-down-freepik {
+    width: 100%;
+    height: 30px;
+    position: absolute;
+    top: 0;
+    bottom: 40px;
+    left: 0;
+    right: 0;
+    margin-top: auto;
+    animation: ${animationRule};
   }
 
   .vagabond-logo {
@@ -24,8 +54,37 @@ export const HeroStyles = styled.div`
     width: 70%;
     height: 30%;
   }
+
   .hero-text-main {
-    font-family: 'VarelaRound';
+    font-family: 'Teko';
+    color: white;
+    font-size: 3.1rem;
+    letter-spacing: 0.14em;
+    margin-top: 20px;
+
+    &::selection {
+      text-shadow: none;
+      color: white;
+      background: black;
+    }
+  }
+
+  .hero-text-sub {
+    font-family: 'Teko';
     color: black;
+    font-size: 2rem;
+    letter-spacing: 0.14em;
+    margin-top: 20px;
+
+    &::selection {
+      text-shadow: none;
+      color: white;
+      background: #2ecc71;
+    }
+
+    span {
+      color: black;
+      font-size: 0.6rem;
+    }
   }
 `;
