3174843a8cd08c9515ee72909e7110245347ee98
falsepopsky <javicream@gmail.com>
2021-01-15 00:53:59 -0300

    test production

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import HeroSection from '../components/hero/Hero';
 import Heatmap from '../components/heatmap/HiatusHeatmap';
 import ChaptersPerYear from '../components/chapters-per-year/ChaptersPerYear';
-import SocialMedia from '../components/twitter/Social';
+import SocialMedia from '../components/social/Social';
 import ChaptersPerArc from '../components/chapters-per-arc/ChaptersPerArc';
-
 import Footer from '../components/footer/Footer';
 
 const Home = () => {
