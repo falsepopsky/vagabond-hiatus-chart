269687f8dc2020b7b3fab93b6823c2c07c998f84
falsepopsky <javicream@gmail.com>
2021-03-22 13:31:13 -0300

    new morning issue #17 and small changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HeroStyles, HeroSectionHeader, HeroSectionSub } from './hero-styled';
 import VagabondLogo from '../../assets/vagabond-jp.svg';
-import ArrowDown from '../../assets/arrow-down.svg';
+import ArrowSVG from './Arrow';
 
 const HeroSection = () => {
   return (
@@ -10,20 +10,14 @@ const HeroSection = () => {
         <h1 className="hero-title">Vagabond Hiatus Chart</h1>
         <figure className="hero-figure">
           <p className="hero-scroll-down">scroll down</p>
-          <img
-            className="hero-arrow"
-            src={ArrowDown}
-            alt="arrow down by Freepik"
-            width="30"
-            height="30"
-          />
+          <ArrowSVG />
         </figure>
       </HeroSectionHeader>
       <HeroSectionSub>
         <img
           className="hero-logo"
           src={VagabondLogo}
-          alt="logo"
+          alt="Vagabond Logo"
           width="681"
           height="280"
         />
