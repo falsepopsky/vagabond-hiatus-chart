d1a69745152ab8e8d97eddb056b54dd0b80ede13
rodrigo <javicream@gmail.com>
2023-08-08 03:51:34 +0000

    chore: update configs

---
+++ sun/tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+const config: Config = {
+  content: ['./app/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
