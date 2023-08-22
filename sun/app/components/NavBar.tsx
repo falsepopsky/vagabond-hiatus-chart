a9b6a07581278856d5c07bab67d33054a55aa43f
rodrigo <javicream@gmail.com>
2023-08-22 21:33:27 +0000

    fix: keep the styles in other pages.

--- sun/app/components/NavBar.tsx
+++ sun/app/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { ToggleTheme } from './ThemeToggle';
 
 export const Navbar = () => (
-  <nav className='absolute right-0 flex h-14 items-center justify-between px-6 text-sm font-medium'>
+  <nav className='absolute right-0 top-0 flex h-14 items-center justify-between px-6 text-sm font-medium'>
     <ToggleTheme />
   </nav>
 );
