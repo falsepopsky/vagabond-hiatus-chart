8b7273bb190fb502bfccb82824642dc2b5d4ba03
rodrigo <falsepopsky@protonmail.com>
2023-08-27 19:47:50 +0000

    fix: prevent going full width and height on render

--- sun/app/components/ThemeToggle.tsx
+++ sun/app/components/ThemeToggle.tsx
@@ -33,10 +33,22 @@ export function ToggleTheme() {
       title='Toggle light & dark color scheme'
       aria-label='Toggle light & dark theme'
       aria-live='polite'>
-      <svg className='hidden h-4 w-4 dark:block' viewBox='0 0 32 32' fill='currentColor' aria-hidden='true'>
+      <svg
+        className='hidden h-4 w-4 dark:block'
+        viewBox='0 0 32 32'
+        fill='currentColor'
+        aria-hidden='true'
+        width={16}
+        height={16}>
         <path d='M13.5025,5.4136A15.0755,15.0755,0,0,0,25.096,23.6082a11.1134,11.1134,0,0,1-7.9749,3.3893c-.1385,0-.2782.0051-.4178,0A11.0944,11.0944,0,0,1,13.5025,5.4136M14.98,3a1.0024,1.0024,0,0,0-.1746.0156A13.0959,13.0959,0,0,0,16.63,28.9973c.1641.006.3282,0,.4909,0a13.0724,13.0724,0,0,0,10.702-5.5556,1.0094,1.0094,0,0,0-.7833-1.5644A13.08,13.08,0,0,1,15.8892,4.38,1.0149,1.0149,0,0,0,14.98,3Z' />
       </svg>
-      <svg className='h-4 w-4 dark:hidden' viewBox='0 0 32 32' fill='currentColor' aria-hidden='true'>
+      <svg
+        className='h-4 w-4 dark:hidden'
+        viewBox='0 0 32 32'
+        fill='currentColor'
+        aria-hidden='true'
+        width={16}
+        height={16}>
         <rect x='15' y='2' width='2' height='5' />
         <rect x='21.6675' y='6.8536' width='4.958' height='1.9998' transform='translate(1.5191 19.3744) rotate(-45)' />
         <rect x='25' y='15' width='5' height='2' />
